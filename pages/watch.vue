f7e353aff14b01dbc9df161fb5640627818a5041
amir daaee <amir.daaee@gmail.com>
2024-09-15 16:16:34 +0330

    responsive bug fix

--- pages/watch.vue
+++ pages/watch.vue
@@ -2,15 +2,15 @@
     <v-container fluid class="my-auto">
         <v-row justify="center">
             <v-col style="max-width: 90vw; width: 100%;" lg="5">
-                <v-sheet>
+                <v-sheet class="overflow-hidden">
                     <div class="text-body-2">{{ vidData?.Media.FileName }}</div>
                     <VideoPlayer :videoSrc="useURL().stream(vidID)"
                         :videoPoster="useURL().thumbnail(vidData?.Media.Thumbnail || '')"
                         :videoTitle="vidData?.Media.FileName" />
                 </v-sheet>
                 <div class="d-flex mt-2">
-                    <v-row justify="space-between">
-                        <v-col v-for="x in [vidData?.Back, vidData?.Next]" col="6" lg="4">
+                    <v-row justify="space-between" class="overflow-hidden">
+                        <v-col v-for="x in [vidData?.Back, vidData?.Next]" col="6" lg="4" class="overflow-hidden">
                             <a :href="useURL().watch(x?.ID || '')" class="text-decoration-none">
                                 <v-img v-if="x?.ID" :src="useURL().thumbnail(x?.Thumbnail || '')" class="align-end px-1"
                                     gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="150px" cover>
@@ -21,7 +21,8 @@
                                     </div>
                                 </v-img>
                             </a>
-                            <div class="text-caption text-grey-darken-2">{{ x?.FileName }}</div>
+                            <div class="text-caption text-grey-darken-2 overflow-hidden text-no-wrap">{{ x?.FileName }}
+                            </div>
                         </v-col>
                     </v-row>
                 </div>
