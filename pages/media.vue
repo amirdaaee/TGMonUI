ad7fccc7280d1f08329fb06b70d15091ba0cae11
amir daaee <amir.daaee@gmail.com>
2024-10-13 11:57:44 +0330

    code improvement

--- pages/media.vue
+++ pages/media.vue
@@ -3,34 +3,34 @@
         <v-card class="mx-auto " max-width="800">
             <v-toolbar density="compact">
                 <v-spacer></v-spacer>
-                <v-btn @click="showThumb = !showThumb" icon>
-                    <v-icon>{{ showThumb ? 'mdi-eye' : 'mdi-eye-off' }}</v-icon>
+                <v-btn @click="pageState.showThumb = !pageState.showThumb" icon>
+                    <v-icon>{{ pageState.showThumb ? 'mdi-eye' : 'mdi-eye-off' }}</v-icon>
                 </v-btn>
             </v-toolbar>
             <v-container fluid>
                 <v-row dense>
                     <v-col v-for="med, n in mediaList?.Media" :key="n" :cols="12" :lg="4">
-                        <media-card :media="med" :show-image="showThumb" :selectable="true" :dlable="true"
-                            v-model:selected="selection[med.ID]" />
+                        <media-card :media="med" :show-image="pageState.showThumb" :selectable="true" :dlable="true"
+                            v-model:selected="pageState.selection[med.ID]" />
                     </v-col>
                 </v-row>
             </v-container>
-            <v-pagination size="" :length="totalPages" v-model="currentPage" total-visible="4"></v-pagination>
-            <v-overlay v-model="loadingState" class="align-center justify-center" persistent contained />
+            <v-pagination size="" :length="totalPages" v-model="pageState.currentPage" total-visible="4"></v-pagination>
+            <v-overlay v-model="pageState.isLoading" class="align-center justify-center" persistent contained />
         </v-card>
-        <delete-modal v-model:display="deleteModelState" :items="selectedItemComputed"
+        <delete-modal v-model:display="pageState.showDeleteModel" :items="selectedItemComputed"
             @confirm="deleteMedia"></delete-modal>
-        <!-- <v-fab size="large" icon="mdi-delete-empty" color="red" v-show="isAnySelected" @click="deleteModelState = true"
-            absoulte app></v-fab> -->
         <v-speed-dial transition="fade-transition" location="top right" :offset="0" v-if="isAnySelected">
             <template v-slot:activator="{ props: activatorProps }">
                 <v-fab v-bind="activatorProps" size="large" icon="$vuetify" color="light-blue-darken-4" absoulte
                     app></v-fab>
             </template>
-            <v-btn key="1" icon="mdi-delete-empty" color="red" @click="deleteModelState = true" absoulte app />
+            <v-btn key="1" icon="mdi-delete-empty" color="red" @click="pageState.showDeleteModel = true" absoulte app />
             <v-btn key="2" icon="mdi-eye" color="yellow" @click="createThumbnail" absoulte app />
         </v-speed-dial>\
-        <v-snackbar v-model="showSnackbar" :timeout="2000" :color="snackbarColor">{{ snackbarText }}</v-snackbar>
+        <v-snackbar v-model="pageState.showSnackbar" :timeout="2000" :color="pageState.snackbarColor">{{
+            pageState.snackbarText
+        }}</v-snackbar>
     </v-main>
 </template>
 
@@ -39,39 +39,51 @@ import { useRouter } from 'vue-router';
 import type { MediaListType, MediaType } from '~/types';
 const router = useRouter()
 // ...
+interface PageState {
+    currentPage: number;
+    showDeleteModel: boolean;
+    showSnackbar: boolean;
+    showThumb: boolean;
+    snackbarColor: string;
+    snackbarText: string;
+    selection: Record<string, boolean>;
+    isLoading: boolean;
+}
+
+// ...
 const pageSize = 9
-const currentPage = ref(Number(useRoute().query.page) || 1)
-const loadingState = ref(false)
-const deleteModelState = ref<boolean>(false)
-const showSnackbar = ref<boolean>(false)
-const snackbarColor = ref<string>("")
-const snackbarText = ref<string>("")
-const showThumb = ref<boolean>(Boolean(useRoute().query.d == "0" ? false : true))
-const selection: { [id: string]: boolean; } = reactive({});
+const pageState = reactive<PageState>({
+    currentPage: Number(useRoute().query.page) || 1,
+    showDeleteModel: false,
+    showSnackbar: false,
+    showThumb: Boolean(useRoute().query.d == "0" ? false : true),
+    snackbarColor: "",
+    snackbarText: "",
+    selection: {},
+    isLoading: false,
+
+
+})
 const totalPages = computed(() => {
     return mediaList.value ? Math.ceil(mediaList.value.Total / pageSize) : 1
 })
 const apiQuery = computed(() => {
-    return { "page": currentPage.value, "page_size": pageSize }
+    return { "page": pageState.currentPage, "page_size": pageSize }
 })
-const { data: mediaList, refresh: mediaListRef } = await useAPI<MediaListType>(useRuntimeConfig().public.baseApi + '/media', {
-    query: apiQuery,
-    onRequest: () => {
-        loadingState.value = true
-    }, onResponse: () => {
-        loadingState.value = false
-    },
-})
+const { data: mediaList, refresh: mediaListRef, status: mediaListStat } = await useAPI<MediaListType>(useRuntimeConfig().public.baseApi + '/media', { query: apiQuery })
 // ...
-watch(currentPage, (newValue) => {
+watch(() => pageState.currentPage, (newValue) => {
     router.push({ query: { page: newValue } })
 })
-watch(showThumb, (newValue) => {
+watch(() => pageState.showThumb, (newValue) => {
     router.push({ query: { d: (newValue ? 1 : 0) } })
 })
+watch(() => [mediaListStat.value], (newValue) => {
+    pageState.isLoading = (newValue[0] == "pending")
+})
 const isAnySelected = computed(() => {
-    for (let key in selection) {
-        if (selection[key]) {
+    for (let key in pageState.selection) {
+        if (pageState.selection[key]) {
             return true
         }
     }
@@ -80,7 +92,7 @@ const isAnySelected = computed(() => {
 const selectedItemComputed = computed(() => {
     let i = <Array<MediaType>>[]
     mediaList.value?.Media.forEach(function (v) {
-        if (selection[v.ID]) {
+        if (pageState.selection[v.ID]) {
             i.push(v)
         }
     })
@@ -88,12 +100,12 @@ const selectedItemComputed = computed(() => {
 })
 // ...
 function clearSelection() {
-    for (let key in selection) {
-        selection[key] = false
+    for (let key in pageState.selection) {
+        pageState.selection[key] = false
     }
 }
 async function deleteMedia() {
-    loadingState.value = true
+    pageState.isLoading = true
     let prmsList = <Array<Promise<any>>>[]
     selectedItemComputed.value.forEach(function (v) {
         prmsList.push(useAPI(useRuntimeConfig().public.baseApi + '/media/' + v.ID, { method: "DELETE" }))
@@ -101,10 +113,10 @@ async function deleteMedia() {
     await Promise.all(prmsList)
     await mediaListRef()
     clearSelection()
-    loadingState.value = false
+    pageState.isLoading = false
 }
 async function createThumbnail() {
-    loadingState.value = true
+    pageState.isLoading = true
     let idList = <Array<string>>[]
     selectedItemComputed.value.forEach(function (v) {
         idList.push(v.ID)
@@ -124,12 +136,12 @@ async function createThumbnail() {
         snack("request sent", "green")
         clearSelection()
     }
-    loadingState.value = false
+    pageState.isLoading = false
 }
 function snack(message: string, color: string) {
-    snackbarColor.value = color
-    snackbarText.value = message
-    showSnackbar.value = true
+    pageState.snackbarColor = color
+    pageState.snackbarText = message
+    pageState.showSnackbar = true
 }
 // ...
 
